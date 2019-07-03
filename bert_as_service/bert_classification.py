b81e9053b0a6a1c2b541a27686b471c1871a7350
llq20133100095 <1182953475@qq.com>
2019-07-03 15:07:29 +0800

    second commit

--- bert_as_service/bert_classification.py
+++ bert_as_service/bert_classification.py
@@ -1,14 +1,22 @@
 # -*- coding: utf-8 -*-
 """
-@time: 2019.7.1
+@time: 2019.7.3
 @author: liluoqin
 @function:
     1.bert embedding in classification
 """
 from bert_serving.client import BertClient
-
+import pandas as pd
+import sklearn
 
 if __name__ == "__main__":
+    # read the all data
+    file_open = open("../data/train_data.csv", encoding="gbk")
+    data = pd.read_csv(file_open)
+    label = data['label']
+    text = data['scontent']
+
+    #get embedding
     bc = BertClient()
-    feature = bc.encode(['First do it', 'then do it right', 'then do it better'])
+    feature = bc.encode(text.to_list()[0:2])
     print(feature.shape)
